b085e1513b747a6220e668f2f5e0c11cb2ac39f6
Jamie Best <james.h.f.best@gmail.com>
2021-01-26 18:02:45 +0000

    Initial Commit

--- creatornetwork/settings.py
+++ creatornetwork/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'creatornetwork.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +128,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
+django_heroku.settings(locals())
--- twentytenlabs/models.py
+++ twentytenlabs/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class creatorDetails(models.Model):
+    creator_id = models.UUIDField(primary_key=True, default=uuid.uuid4,editable=False)
+    creator_name = models.CharField(max_length=75)
+    primary_content_category = models.CharField(max_length=100)
+    secondary_content_category = models.CharField(max_length=100)
+    posts_per_week_ig = models.IntegerField()
+    rate_card = models.DecimalField(max_digits=10, decimal_places=2)
+    ig_audience = models.IntegerField()
+    fb_audience = models.IntegerField()
+    tw_audience = models.IntegerField()
+    yt_audience = models.IntegerField()
+
+    def __str__(self):
+        return '{}'.format(self.creator_name)
+
+class creatorProfiles(models.Model):
+    pass
+
+class creatorPerformance(models.Model):
+    pass
+
+class creatorSocialStats(models.Model):
+    pass
+
+class youtubeStats(models.Model):
+    pass
+
+class creatorBio(models.Model):
+    pass
